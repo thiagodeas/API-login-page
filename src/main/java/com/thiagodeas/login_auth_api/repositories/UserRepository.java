6e562d27a180fa09729eda997c649ebcef583453
thiagodeas <sousaalvesth@gmail.com>
2024-11-29 09:55:27 -0300

    criando Security Filter

--- src/main/java/com/thiagodeas/login_auth_api/repositories/UserRepository.java
+++ src/main/java/com/thiagodeas/login_auth_api/repositories/UserRepository.java
@@ -3,5 +3,8 @@ package com.thiagodeas.login_auth_api.repositories;
 import com.thiagodeas.login_auth_api.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByEmail(String email);
 }
